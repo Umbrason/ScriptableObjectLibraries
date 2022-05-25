5bab0cf8e3988cc2be1ac0873f44a4b8ee965a6f
Umbrason <FabioKolligs@outlook.de>
2022-05-26 01:32:29 +0200

    cache derived types
    caches derived types on opening the inspector

--- Editor/ComponentObjectLibraryEditor.cs
+++ Editor/ComponentObjectLibraryEditor.cs
@@ -6,18 +6,20 @@ using UnityEngine;
 public abstract class ComponentObjectLibraryEditor<Key, Value> : UnityEditor.Editor where Key : MonoBehaviour where Value : UnityEngine.Object
 {
     private ComponentObjectLibrary<Key, Value> library;
+    private Type[] derivedTypes;
 
     void OnEnable()
     {
         library = target as ComponentObjectLibrary<Key, Value>;
+        derivedTypes = System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(x=>x.GetTypes()).Where((t) => (!t.IsAbstract) && (!t.IsGenericType) && t.IsSubclassOf(typeof(Key))).Distinct().ToArray();
+        derivedTypes.OrderBy(x => x.Name);
     }
 
     public override void OnInspectorGUI()
     {
         if (library == null)
             return;
-        var derivedTypes = System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(x=>x.GetTypes()).Where((t) => (!t.IsAbstract) && (!t.IsGenericType) && t.IsSubclassOf(typeof(Key))).Distinct().ToArray();
-        derivedTypes.OrderBy(x => x.Name);
+
         foreach (var type in derivedTypes)
             library[type] = EditorGUILayout.ObjectField(type.Name, library[type], typeof(Key), allowSceneObjects: false) as Value;
         if (GUI.changed)
