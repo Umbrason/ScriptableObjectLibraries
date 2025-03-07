21d802ead5b8d4274aad000116b7e038e39d1208
Umbrason <Umbrason@Outlook.com>
2025-03-07 17:27:21 +0100

    Update TypeObjectLibrary.cs

--- Runtime/TypeObjectLibrary.cs
+++ Runtime/TypeObjectLibrary.cs
@@ -24,34 +24,36 @@ public abstract class TypeObjectLibrary<KeyBaseClass, Value> : ScriptableObject,
     [SerializeField] private string[] _keys;
     [SerializeField] private Value[] _values;
 
-    public Dictionary<Type, Value> componentObjects = new Dictionary<Type, Value>();
+    public readonly Dictionary<string, Value> componentObjects = new();
     public Value this[Type t]
     {
         get
         {
-            return componentObjects.TryGetValue(t, out Value value) ? value : default;
+            return componentObjects.TryGetValue(t.Name, out Value value) ? value : default;
         }
         set
         {
             if ((t.IsSubclassOf(typeof(KeyBaseClass)) || t.GetInterfaces().Contains(typeof(KeyBaseClass))) && !t.IsGenericType && !t.IsAbstract)
-                componentObjects[t] = value;
+                componentObjects[t.Name] = value;
             else Debug.Log(t.Name);
         }
     }
 
     public void OnAfterDeserialize()
     {
-        componentObjects = new Dictionary<Type, Value>();
+        componentObjects.Clear();
         if (_keys != null && _values != null)
             for (int i = 0; i < Mathf.Min(_keys.Length, _values.Length); i++)
                 if (_keys[i] != null)
-                    componentObjects.Add(Type.GetType(_keys[i]), _values[i]);
+                {
+                    componentObjects.Add(_keys[i], _values[i]);
+                }
     }
 
     public void OnBeforeSerialize()
     {
         var keyTypes = componentObjects.Keys.ToArray();
-        _keys = componentObjects.Keys.Select(t => $"{t.AssemblyQualifiedName}|{t.Assembly.FullName}").ToArray();
+        _keys = componentObjects.Keys.ToArray();
         _values = keyTypes.Select(key => componentObjects[key]).ToArray();
-    }    
+    }
 }
